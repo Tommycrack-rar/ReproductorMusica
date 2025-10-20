9b6d1d77d32a7e76fff6567d2a7f904886a1ba9b
JOHAN247 <johmunozma@unal.edu.co>
2025-10-20 08:39:30 -0500

    Update MainFrame.cpp

--- ReproductorMusica/MainFrame.cpp
+++ ReproductorMusica/MainFrame.cpp
@@ -1,6 +1,6 @@
 /**
  * @file MainFrame.cpp
- * @author JOHAN MUÑOZ (johmunozma@unal.edu.co) and Tomas Suarez()
+ * JOHAN MUÑOZ (johmunozma@unal.edu.co) and TOMAS SUAREZ()
  * @brief Clase principal que representa la ventana del reproductor de música
  * @version 0.1
  * @date 2025-10-20
